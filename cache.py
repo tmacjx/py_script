9154fd85876778eb8c8c205cf09ee756ee0e1682
tmackan <wangk@bokecc.com>
2019-12-26 20:12:37 +0800

    cache相关

---
+++ cache.py
@@ -0,0 +1,41 @@
+"""
+# @Author  wk
+# @Time 2019/12/26 20:11
+
+"""
+import functools
+
+
+class CacheProperty(object):
+    """
+    Decorator that converts a method with a single self argument into a
+    property cached on the instance.
+
+    Optional ``name`` argument allows you to make cached properties of other
+    methods. (e.g.  url = cached_property(get_absolute_url, name='url') )
+    """
+    def __init__(self, func, name=None):
+        self.func = func
+        self.__doc__ = getattr(func, '__doc__')
+        self.name = name or func.__name__
+
+    def __get__(self, instance, type=None):
+        if instance is None:
+            return self
+        res = instance.__dict__[self.name] = self.func(instance)
+        return res
+
+
+def memoize(obj):
+    """
+    Local cache of the function return value
+    """
+    cache = obj.cache = {}
+
+    @functools.wraps(obj)
+    def memoizer(*args, **kwargs):
+        key = str(args) + str(kwargs)
+        if key not in cache:
+            cache[key] = obj(*args, **kwargs)
+        return cache[key]
+    return memoizer
